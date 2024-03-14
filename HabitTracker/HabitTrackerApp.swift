3f886890c9685c0842cd4dcf0be5d1ec00ac0ad1
Nikolai Nobadi <nikolai.nobadi@gmail.com>
2024-03-14 09:33:36 -0700

    Initial Commit

---
+++ HabitTracker/HabitTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  HabitTrackerApp.swift
+//  HabitTracker
+//
+//  Created by Nikolai Nobadi on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct HabitTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
